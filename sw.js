1934e75fe2c0205589a5e4e462cf6d012997d2c0
oifromlip <vfh79b79x5@privaterelay.appleid.com>
2026-05-05 10:43:09 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'budget-v20';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
